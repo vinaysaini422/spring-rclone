975c335ab22dfb8a3d461226e628694cebcd0ca3
vinaysaini422@gmail.com <vinaysaini422@gmail.com>
2020-05-30 00:22:16 +0530

    adding swagger

--- src/main/java/com/saini/rclone/SpringMicroApplication.java
+++ src/main/java/com/saini/rclone/SpringMicroApplication.java
@@ -1,9 +1,12 @@
 package com.saini.rclone;
 
+import com.saini.rclone.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class SpringMicroApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/saini/rclone/config/SecurityConfig.java
+++ src/main/java/com/saini/rclone/config/SecurityConfig.java
@@ -39,6 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.authorizeRequests()
 			.antMatchers("/api/auth/**")
 			.permitAll()
+			.antMatchers("/v2/api-docs",
+				"/configuration/ui",
+				"/swagger-resources/**",
+				"/configuration/security",
+				"/swagger-ui.html",
+				"/webjars/**")
+			.permitAll()
 			.antMatchers(HttpMethod.GET, "/api/subreddit")
 			.permitAll()
 			.anyRequest()
